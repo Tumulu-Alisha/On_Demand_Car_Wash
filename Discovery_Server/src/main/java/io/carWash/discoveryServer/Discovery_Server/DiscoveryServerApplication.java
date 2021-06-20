fb0be3c61177e790063d3639477d07915bacd9ac
Tumulu Alisha <tumulualisha45@gmail.com>
2021-06-20 15:48:48 +0530

    Discovery Server added

---
+++ Discovery_Server/src/main/java/io/carWash/discoveryServer/Discovery_Server/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package io.carWash.discoveryServer.Discovery_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.*;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
