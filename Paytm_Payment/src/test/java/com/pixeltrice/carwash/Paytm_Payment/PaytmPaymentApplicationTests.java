8bd224537523394a9a0b5f52cc83fb38cbad372d
Tumulu Alisha <tumulualisha45@gmail.com>
2021-06-20 15:49:46 +0530

    Payment Gateway added

---
+++ Paytm_Payment/src/test/java/com/pixeltrice/carwash/Paytm_Payment/PaytmPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixeltrice.carwash.Paytm_Payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
